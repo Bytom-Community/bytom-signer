1012de53bff7c2d5988590215168cf5baa3ec91e
HAOYUatHZ <haoyu@protonmail.com>
2018-12-05 10:19:30 +0800

    Add vendor

--- main.go
+++ main.go
@@ -1,18 +1,22 @@
 package main
 
 import (
-	"fmt"
+// "fmt"
+)
+
+var (
+	mnemonic = ""
 )
 
 func main() {
-	xpub, err := importKeyFromMnemonic( /*in.Alias, in.Password, */ in.Mnemonic)
+	xpub, err := importKeyFromMnemonic( /*in.Alias, in.Password, */ mnemonic)
 	if err != nil {
 		return
 	}
 	return
 
-	if err := txbuilder.Sign(ctx, &x.Txs, x.Password, a.pseudohsmSignTemplate); err != nil {
-		log.WithField("build err", err).Error("fail on sign transaction.")
-		return
-	}
+	// if err := txbuilder.Sign(ctx, &x.Txs, x.Password, a.pseudohsmSignTemplate); err != nil {
+	// 	log.WithField("build err", err).Error("fail on sign transaction.")
+	// 	return
+	// }
 }
--- mnemonic.go
+++ mnemonic.go
@@ -1,29 +1,30 @@
 package main
 
-func importKeyFromMnemonic() (*XPub, error) {
+import (
+	"strings"
+
+	mnem "github.com/bytom/wallet/mnemonic"
+)
+
+// EntropyLength random entropy length to generate mnemonics.
+const EntropyLength = 128
+
+var (
+	ErrMnemonicLength = errors.New("mnemonic length error")
+)
+
+func importKeyFromMnemonic(mnemonic string) (*XPub, error) {
 	// checksum length = entropy length /32
 	// mnemonic length = (entropy length + checksum length)/11
 	if len(strings.Fields(mnemonic)) != (EntropyLength+EntropyLength/32)/11 {
 		return nil, ErrMnemonicLength
 	}
 
-	normalizedAlias := strings.ToLower(strings.TrimSpace(alias))
-	if ok := h.cache.hasAlias(normalizedAlias); ok {
-		return nil, ErrDuplicateKeyAlias
-	}
-
 	// Pre validate that the mnemonic is well formed and only contains words that
 	// are present in the word list
-	if !mnem.IsMnemonicValid(mnemonic, language) {
+	if !mnem.IsMnemonicValid(mnemonic, "en") {
 		return nil, mnem.ErrInvalidMnemonic
 	}
 
-	xpub, err := h.createKeyFromMnemonic(alias, auth, mnemonic)
-	if err != nil {
-		return nil, err
-	}
-
-	h.cache.add(*xpub)
-	return xpub, nil
-
+	return createKeyFromMnemonic(alias, auth, mnemonic)
 }
